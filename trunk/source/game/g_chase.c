8c61932315df3eb65d41fb66de86ff576dff5acf
irritant <irritant@example.com>
2007-03-22 22:24:48 +0000

    Test of chasecam

--- trunk/source/game/g_chase.c
+++ trunk/source/game/g_chase.c
@@ -39,7 +39,7 @@ void UpdateChaseCam(edict_t *ent)
 		ChaseNext(ent);
 		if (ent->client->chase_target == old) {
 			ent->client->chase_target = NULL;
-			ent->client->ps.pmove.pm_flags &= ~PMF_NO_PREDICTION;
+			//ent->client->ps.pmove.pm_flags &= ~PMF_NO_PREDICTION;
 			return;
 		}
 	}
@@ -100,7 +100,7 @@ void UpdateChaseCam(edict_t *ent)
 	VectorCopy(targ->client->v_angle, ent->client->v_angle);
 
 	ent->viewheight = 0;
-	ent->client->ps.pmove.pm_flags |= PMF_NO_PREDICTION;
+//	ent->client->ps.pmove.pm_flags |= PMF_NO_PREDICTION;
 	gi.linkentity(ent);
 }
 
