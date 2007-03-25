367c35ec8988c9daac5b441436d3599c715cf65d
tony <tony@example.com>
2007-03-25 20:24:20 +0000

    Extra safety check put into place around fix for broken client names

--- trunk/source/game/p_client.c
+++ trunk/source/game/p_client.c
@@ -2057,7 +2057,8 @@ void ClientUserinfoChanged (edict_t *ent, char *userinfo, int whereFrom)
 	// set name
 	s = Info_ValueForKey (userinfo, "name");
 	//fix player name if corrupted
-	s[16] = 0;
+	if(s != NULL && strlen(s) > 16)
+		s[16] = 0;
 	strncpy (ent->client->pers.netname, s, sizeof(ent->client->pers.netname)-1);
 
 	//spectator mode
