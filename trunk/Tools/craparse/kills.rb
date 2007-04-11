40ef2fd902c762a5d7ec7fabca4b2c9c695795c4
phenax <phenax@example.com>
2007-04-11 01:09:31 +0000

    Cleanup: Added a hash for kill strings, made the defs return the length instead of the string. Modified Kills.all() to Kills.total() and made it not use length since the defs now return the length

--- trunk/Tools/craparse/kills.rb
+++ trunk/Tools/craparse/kills.rb
@@ -1,92 +1,108 @@
+$h = Hash.new
+$h['blaster'] = /was blasted by #{$player}/
+$h['chaingunBurst'] = /was blown away by #{$player}'s chaingun burst/
+$h['chaingun'] = /was cut in half by #{$player}'s chaingun/
+$h['flame'] = /was burned by #{$player}'s napalm/
+$h['rocket'] = /ate #{$player}'s rocket/
+$h['rocketSplash'] = /almost dodged #{$player}'s rocket/
+$h['beamgun'] = /was melted by #{$player}'s beamgun/
+$h['disruptor'] = /was disrupted by #{$player}/
+$h['smartgun'] = /saw the pretty lights from #{$player}'s smartgun/
+$h['vaporizer'] = /was disintegrated by #{$player}'s vaporizer blast/
+$h['vaporizerAlt'] = /couldn't hide from #{$player}'s vaporizer/
+$h['plasmaSplash'] = /was melted by #{$player}'s plasma/
+$h['telefrag'] = /tried to invade #{$player}'s personal space/
+$h['grapple'] = /was caught by #{$player}'s grapple/
+
 class Kills
 	def initialize
 	end
 
 	def Kills.blaster()	
 		File.open($file) do |op|
-			op.grep(/was blasted by #{$player}/)
+			op.grep($h['blaster']).length
 		end
 	end
 
 	def Kills.chaingunBurst()
 		File.open($file) do |op|
-			op.grep(/was blown away by #{$player}'s chaingun burst/)
+			op.grep($h['chaingunBurst']).length
 		end
 	end
 
 	def Kills.chaingun()
 		File.open($file) do |op|
-			op.grep(/was cut in half by #{$player}'s chaingun/)
+			op.grep($h['chaingun']).length
 		end
 	end
 
 	def Kills.flame()
 		File.open($file) do |op|
-			op.grep(/was burned by #{$player}'s napalm/)
+			op.grep($h['flame']).length
 		end
 	end
 
 	def Kills.rocket()
 		File.open($file) do |op|
-			op.grep(/ate #{$player}'s rocket/)
+			op.grep($h['rocket']).length
 		end
 	end
 	
 	def Kills.rocketSplash()
 		File.open($file) do |op|
-			op.grep(/almost dodged #{$player}'s rocket/)
+			op.grep($h['rocketSplash']).length
 		end
 	end
 
 	def Kills.beamgun()
 		File.open($file) do |op|
-			op.grep(/was melted by #{$player}'s beamgun/)
+			op.grep($h['beamgun']).length
 		end
 	end
 
 	def Kills.disruptor()
 		File.open($file) do |op|
-			op.grep(/was disrupted by #{$player}/)
+			op.grep($h['disruptor']).length
 		end
 	end
 
 	def Kills.smartgun()
 		File.open($file) do |op|
-			op.grep(/saw the pretty lights from #{$player}'s smartgun/)
+			op.grep($h['smartgun']).length
 		end
 	end
 
 	def Kills.vaporizer()
 		File.open($file) do |op|
-			op.grep(/was disintegrated by #{$player}'s vaporizer blast/)
+			op.grep($h['vaporizer']).length
 		end
 	end
 
 	def Kills.vaporizerAlt()
 		File.open($file) do |op|
-			op.grep(/couldn't hide from #{$player}'s vaporizer/)
+			op.grep($h['vaporizerAlt']).length
 		end
 	end
 
 	def Kills.plasmaSplash()
 		File.open($file) do |op|
-			op.grep(/was melted by #{$player}'s plasma/)
+			op.grep($h['plasmaSplash']).length
 		end
 	end
 
 	def Kills.telefrag()
 		File.open($file) do |op|
-			op.grep(/tried to invade #{$player}'s personal space/)
+			op.grep($h['telefrag']).length
 		end
 	end
 
 	def Kills.grapple()
 		File.open($file) do |op|
-			op.grep(/was caught by #{$player}'s grapple/)
+			op.grep($h['grapple']).length
 		end
 	end
 
-	def Kills.all()
-		Kills.blaster().length + Kills.chaingunBurst().length + Kills.chaingun().length + Kills.flame().length + Kills.rocket().length + Kills.rocketSplash().length + Kills.beamgun().length + Kills.disruptor().length + Kills.smartgun().length + Kills.vaporizer().length + Kills.vaporizerAlt().length + Kills.plasmaSplash().length + Kills.telefrag().length + Kills.grapple().length
+	def Kills.total()
+		Kills.blaster() + Kills.chaingunBurst() + Kills.chaingun() + Kills.flame() + Kills.rocket() + Kills.rocketSplash() + Kills.beamgun() + Kills.disruptor() + Kills.smartgun() + Kills.vaporizer() + Kills.vaporizerAlt() + Kills.plasmaSplash() + Kills.telefrag() + Kills.grapple()
 	end
 end
