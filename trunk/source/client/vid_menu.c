7bc2fbb51013920e038227d8b30b27242cc429ed
phenax <phenax@example.com>
2007-07-04 13:48:19 +0000

    Change ../client/*.h to *.h

--- trunk/source/client/vid_menu.c
+++ trunk/source/client/vid_menu.c
@@ -17,8 +17,8 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 */
-#include "../client/client.h"
-#include "../client/qmenu.h"
+#include "client.h"
+#include "qmenu.h"
 
 extern cvar_t *vid_ref;
 extern cvar_t *vid_fullscreen;
