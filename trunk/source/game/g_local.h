3b620b74701a3ebd94b24a3ca50a2260688a251d
phenax <phenax@example.com>
2007-07-12 13:01:32 +0000

    'fuse' the seperate instagib damage cvars.

--- trunk/source/game/g_local.h
+++ trunk/source/game/g_local.h
@@ -581,8 +581,6 @@ extern	cvar_t	*wep_selfdmgmulti;
 
 extern	cvar_t	*wep_disruptor_dmg;
 extern	cvar_t	*wep_disruptor_kick;
-extern	cvar_t	*wep_disruptor_insta_dmg;
-extern	cvar_t	*wep_disruptor_insta_kick;
 
 //health/max health/max ammo
 extern	cvar_t	*g_spawnhealth;
