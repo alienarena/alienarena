660388d4b26012fc8495662289e7d45fe8e2a1b1
phenax <phenax@example.com>
2007-09-03 01:45:26 +0000

    oops

--- trunk/source/ref_gl/r_misc.c
+++ trunk/source/ref_gl/r_misc.c
@@ -23,7 +23,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include <GL/glu.h>
 
 //For screenshots
-#include "png.h"
+#include <png.h>
 #include <jpeglib.h>
 
 image_t *r_flare;
