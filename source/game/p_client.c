74a3e3794c4dfadcc1fd85ad70ba57a4a659135f
irritant <irritant@example.com>
2007-11-01 01:01:10 +0000

    Dead bodies needed usegib code added.

--- source/game/p_client.c
+++ source/game/p_client.c
@@ -1316,7 +1316,9 @@ void InitBodyQue (void)
 
 void body_die (edict_t *self, edict_t *inflictor, edict_t *attacker, int damage, vec3_t point)
 {
-	int	n;
+	int	n;	
+	int gib_effect = EF_GREENGIB;
+	
 	self->s.modelindex3 = 0;    //remove helmet, if a martian
 	self->s.modelindex4 = 0;
 	if (self->health < -40)
@@ -1324,9 +1326,10 @@ void body_die (edict_t *self, edict_t *inflictor, edict_t *attacker, int damage,
 		if(self->ctype == 0) { //alien
 
 			for (n= 0; n < 4; n++)
-				ThrowGib (self, "models/objects/gibs/mart_gut/tris.md2", damage, GIB_ORGANIC, EF_GREENGIB);
+				ThrowGib (self, "models/objects/gibs/mart_gut/tris.md2", damage, GIB_METALLIC, EF_GREENGIB);
 		}
 		else if(self->ctype == 2) { //robot
+			gib_effect = 0;
 			for (n= 0; n < 4; n++)
 				ThrowGib (self, "models/objects/debris3/tris.md2", damage, GIB_METALLIC, 0);
 			for (n= 0; n < 4; n++)
@@ -1339,9 +1342,20 @@ void body_die (edict_t *self, edict_t *inflictor, edict_t *attacker, int damage,
 
 		}
 		else { //human
+			gib_effect = EF_GIB;
 			for (n= 0; n < 4; n++)
-				ThrowGib (self, "models/objects/gibs/sm_meat/tris.md2", damage, GIB_ORGANIC, EF_GIB);
+				ThrowGib (self, "models/objects/gibs/sm_meat/tris.md2", damage, GIB_METALLIC, EF_GIB);
 		}
+
+		if(self->usegibs) {
+			ThrowGib (self, self->head, damage, GIB_ORGANIC, gib_effect);
+			ThrowGib (self, self->leg, damage, GIB_ORGANIC, gib_effect);
+			ThrowGib (self, self->leg, damage, GIB_ORGANIC, gib_effect);
+			ThrowGib (self, self->body, damage, GIB_ORGANIC, gib_effect);
+			ThrowGib (self, self->arm, damage, GIB_ORGANIC, gib_effect);
+			ThrowGib (self, self->arm, damage, GIB_ORGANIC, gib_effect);
+		}
+
 		self->s.origin[2] -= 48;
 		ThrowClientHead (self, damage);
 		self->takedamage = DAMAGE_NO;
