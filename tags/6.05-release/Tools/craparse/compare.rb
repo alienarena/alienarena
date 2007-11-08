6aaf4c603621b1a7f11da8a88027347296458fe3
phenax <phenax@example.com>
2007-11-08 23:47:22 +0000

    Tagged wrong, used actual files. Fix later

--- tags/6.05-release/Tools/craparse/compare.rb
+++
@@ -1,9 +0,0 @@
-class Compare < String
-	include Comparable
-	def <=>(other)
-		self.to_i <=> other.to_i
-	end
-	def to_i
-		self.scan(/\d+/)[0].to_s.to_i
-	end
-end
--- tags/6.05-release/data1/maps/compile.bat
+++
@@ -1,4 +0,0 @@
-qbsp3 dm-frontier2
-qvis3 dm-frontier2
-qrad3 dm-frontier2
-pause
