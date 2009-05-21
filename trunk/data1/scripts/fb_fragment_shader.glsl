00b6996af826841d66441df04b009cbe220f2368
irritant <irritant@example.com>
2009-05-21 19:13:58 +0000

    Fixed shearing artifacts on edges of screen with fb shaders.

--- trunk/data1/scripts/fb_fragment_shader.glsl
+++ trunk/data1/scripts/fb_fragment_shader.glsl
@@ -10,7 +10,7 @@ void main(void)
 	vec3 noiseVec;
 	vec2 displacement;
 
-    	displacement = gl_TexCoord[0].st;
+    displacement = gl_TexCoord[0].st;
 
 	displacement.x -= fxPos.x*0.002;
 	displacement.y -= fxPos.y*0.002;
@@ -18,7 +18,18 @@ void main(void)
 	noiseVec = normalize(texture2D(distortiontexture, displacement.xy)).xyz;
 	noiseVec = (noiseVec * 2.0 - 0.635) * 0.035;
 
-	gl_FragColor = texture2D(fbtexture, gl_TexCoord[0].st + noiseVec.xy);
+	//clamp edges to prevent artifacts
+	if(gl_TexCoord[0].s > 0.1 && gl_TexCoord[0].s < 0.6)
+		displacement.x = gl_TexCoord[0].s + noiseVec.x;
+	else
+		displacement.x = gl_TexCoord[0].s;
+		
+	if(gl_TexCoord[0].t > 0.1 && gl_TexCoord[0].t < 0.6)
+		displacement.y = gl_TexCoord[0].t + noiseVec.y;
+	else
+		displacement.y = gl_TexCoord[0].t;
+		
+	gl_FragColor = texture2D(fbtexture, displacement.xy);
 
 	if(fxType == 2)
 		gl_FragColor = mix(gl_FragColor, vec4(fxColor, 1.0), 0.3);
