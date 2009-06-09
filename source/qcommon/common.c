10caf78a4b09dae879437975d8afc603f609e64b
irritant <irritant@example.com>
2009-06-09 19:42:35 +0000

    Added VC6 project, since this compiles faster than MSVC2008, along with the compatible libraries.

--- source/qcommon/common.c
+++ source/qcommon/common.c
@@ -29,6 +29,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #ifndef DEDICATED_ONLY
 extern void S_StartLocalSound (char *sound);
 extern void S_StartMusic ( char * sound );
+extern void S_StartMenuMusic( void );
 #endif
 
 extern void CL_Disconnect(void);
