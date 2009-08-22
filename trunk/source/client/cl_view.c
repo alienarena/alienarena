a84b893f18a38ada0619df13b4c18f722e0d9161
strat <strat@example.com>
2009-08-22 01:08:00 +0000

    Remove unhelpful mod for MS VC9 compiler issue.

--- trunk/source/client/cl_view.c
+++ trunk/source/client/cl_view.c
@@ -571,14 +571,9 @@ float CalcFov (float fov_x, float width, float height)
 	if (fov_x < 1 || fov_x > 179)
 		Com_Error (ERR_DROP, "Bad fov: %f", fov_x);
 
-#ifdef __unix__
-	x = ( height / width ) * tanf( HALFDEG2RAD( fov_x ) );
-	a = DOUBLERAD2DEG( atanf( x ) );
-#else
 	x = width/tan(fov_x/360*M_PI);
     a = atan (height/x);   
     a = a*360/M_PI; 
-#endif
 
 	return a;
 }
