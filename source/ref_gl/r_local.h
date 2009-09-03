661f8609dbf06f9f9c0ba11e47614b2d54803649
irritant <irritant@example.com>
2009-09-03 21:36:32 +0000

    Return normal array to static.

--- source/ref_gl/r_local.h
+++ source/ref_gl/r_local.h
@@ -486,7 +486,7 @@ extern float	norm_array[MAX_ARRAY][3];
 extern float VArrayVerts[MAX_VARRAY_VERTS * MAX_VARRAY_VERTEX_SIZE];
 extern int VertexSizes[];
 extern float *VArray;
-extern vec3_t NormalsArray[MAX_TRIANGLES*3];
+static vec3_t NormalsArray[MAX_TRIANGLES*3];
 extern vec3_t ShadowArray[MAX_SHADOW_VERTS];
 
 // define our vertex types
