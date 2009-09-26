95b46ad92216fd74d8307aa6be657c90ebe39694
irritant <irritant@example.com>
2009-09-26 05:50:45 +0000

    Moved revision # to 7.31 Cleanup of unused maps, fixing ctf-frost2k9(renamed to ctf-frostbyte).

--- trunk/source/qcommon/qcommon.h
+++ trunk/source/qcommon/qcommon.h
@@ -23,7 +23,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "../game/q_shared.h"
 
 
-#define	VERSION		7.30
+#define	VERSION		7.31
 
 #define	BASEDIRNAME	"data1"
 
