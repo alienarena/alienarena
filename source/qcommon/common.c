d159d0a8a51a8313606ce288013674b2147847a1
irritant <irritant@example.com>
2009-10-14 04:45:59 +0000

    More fixes for driver(this one is just an extra precaution).

--- source/qcommon/common.c
+++ source/qcommon/common.c
@@ -22,7 +22,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 #include <setjmp.h>
 
-#define	MAXPRINTMSG	4096
+#define	MAXPRINTMSG	8192
 
 #define MAX_NUM_ARGVS	50
 
