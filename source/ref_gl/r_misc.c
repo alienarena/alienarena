528d468f0b9b67869a1e464a85b1daf4de9e09df
irritant <irritant@example.com>
2009-11-24 23:09:37 +0000

    Removed shell normalmap.

--- source/ref_gl/r_misc.c
+++ source/ref_gl/r_misc.c
@@ -49,7 +49,6 @@ image_t		*r_leaderfieldtexture;
 image_t		*r_deathfieldtexture;
 image_t		*r_deathfieldtexture2;
 image_t		*r_shelltexture;   
-image_t		*r_shellnormal; 
 image_t		*r_hittexture;
 image_t		*r_bubbletexture;
 image_t		*r_reflecttexture;
@@ -271,10 +270,6 @@ void R_InitParticleTexture (void)
 	if (!r_shelltexture) {                                
 		r_shelltexture = GL_LoadPic ("***particle***", (byte *)data, 16, 16, it_sprite, 32);
     }   
-	r_shellnormal = R_RegisterParticlePic("shell_normal");
-	if (!r_shellnormal) {                                
-		r_shellnormal = GL_LoadPic ("***particle***", (byte *)data, 16, 16, it_sprite, 32);
-    }  
 	r_hittexture = R_RegisterParticlePic("aflash");
 	if (!r_hittexture) {                                
 		r_hittexture = GL_LoadPic ("***particle***", (byte *)data, 16, 16, it_sprite, 32);
