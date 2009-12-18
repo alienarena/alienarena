bf7df93f6184bfcb3ce1968e49cf08d2dfc2399c
irritant <irritant@example.com>
2009-12-18 22:09:51 +0000

    Proper ATTN.

--- trunk/source/game/p_client.c
+++ trunk/source/game/p_client.c
@@ -474,7 +474,7 @@ void ClientObituary (edict_t *self, edict_t *inflictor, edict_t *attacker)
 						if(mod == MOD_HEADSHOT) { //3 more pts for a headshot
 							attacker->client->resp.reward_pts+=3;
 							safe_centerprintf(attacker, "HEADSHOT!\n");
-							gi.sound(attacker, CHAN_VOICE, gi.soundindex("misc/headshot.wav"), 1, ATTN_NONE, 0);  
+							gi.sound(attacker, CHAN_AUTO, gi.soundindex("misc/headshot.wav"), 1, ATTN_STATIC, 0);  
 						}
 
 						//mutators
