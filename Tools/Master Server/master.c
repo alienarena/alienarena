def658f361395dcd3f660ae9238e2bd91f29c8e8
strat <strat@example.com>
2010-01-08 04:07:01 +0000

    Add missing #include for Master Server

--- Tools/Master Server/master.c
+++ Tools/Master Server/master.c
@@ -33,9 +33,11 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include <time.h>
 #else
 #include <stdio.h>
+#include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/time.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
