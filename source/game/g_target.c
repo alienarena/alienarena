c9941eb3e19ec691e1d541540e1a976bcd30e309
irritant <irritant@example.com>
2010-01-26 00:15:03 +0000

    Return target_changelevel, it has uses.

--- source/game/g_target.c
+++ source/game/g_target.c
@@ -581,6 +581,54 @@ void SP_target_laser (edict_t *self)
 	self->nextthink = level.time + 1;
 }
 
+/*QUAKED target_changelevel (1 0 0) (-8 -8 -8) (8 8 8)   
+Changes level to "map" when fired   
+*/   
+void use_target_changelevel (edict_t *self, edict_t *other, edict_t *activator)   
+{   
+	if (level.intermissiontime)   
+		return;         // already activated   
+     
+    if (!deathmatch->value)   
+    {   
+		if (g_edicts[1].health <= 0)   
+			return;   
+    }  
+     
+    // if noexit, do a ton of damage to other   
+    if (deathmatch->value && !( (int)dmflags->value & DF_ALLOW_EXIT) && other != world)   
+    {   
+		T_Damage (other, self, self, vec3_origin, other->s.origin, vec3_origin, 10 * other->max_health, 1000, 0, MOD_EXIT);   
+        return;   
+    }   
+     
+    // if multiplayer, let everyone know who hit the exit   
+    if (deathmatch->value)   
+    {   
+		if (activator && activator->client)   
+			safe_bprintf (PRINT_HIGH, "%s exited the level.\n", activator->client->pers.netname);   
+    } 
+     
+	// if going to a new unit, clear cross triggers   
+    if (strstr(self->map, "*"))   
+		game.serverflags &= ~(SFL_CROSS_TRIGGER_MASK);   
+     
+    BeginIntermission (self);   
+}   
+     
+void SP_target_changelevel (edict_t *ent)   
+{   
+	if (!ent->map)   
+    {   
+		gi.dprintf("target_changelevel with no map at %s\n", vtos(ent->s.origin));   
+        G_FreeEdict (ent);   
+        return;   
+    }   
+     
+    ent->use = use_target_changelevel;   
+    ent->svflags = SVF_NOCLIENT;   
+}  
+
 //==========================================================
 
 /*QUAKED target_lightramp (0 .5 .8) (-8 -8 -8) (8 8 8) TOGGLE
