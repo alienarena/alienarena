ae7b71aae1910404153fb8c7e8cfbcc9dac8be13
strat <strat@example.com>
2010-05-02 18:36:49 +0000

    Added add'l DATADIR support. Fixed a test for empty string.

--- trunk/source/client/cl_scrn.c
+++ trunk/source/client/cl_scrn.c
@@ -1306,7 +1306,7 @@ void SCR_ExecuteLayoutString (char *s)
 				value = cl.frame.playerstate.stats[atoi(token)];
 				if (value >= MAX_IMAGES)
 					Com_Error (ERR_DROP, "Pic >= MAX_IMAGES");
-				if (cl.configstrings[CS_IMAGES+value])
+				if( cl.configstrings[CS_IMAGES+value][0] )
 				{
 					SCR_AddDirtyPoint (x, y);
 					SCR_AddDirtyPoint (x+23*scale, y+23*scale);
