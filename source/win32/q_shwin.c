67487ca9366805de4c8f14c4a04f0bfdead7c9cc
strat <strat@example.com>
2010-08-22 17:58:18 +0000

    Convert win32/ sources for vs2010 and autotools build. sys_win.c: Add support for static link of game library. Add partial and tentative vs2010 build changes for ragdoll and ode library. Including ode library .zip. Remove non-Windows conditional compile cruft. Cleanup for compiler warnings. (note: last commit messed up version tracking on default.cfg.)

--- source/win32/q_shwin.c
+++ source/win32/q_shwin.c
@@ -22,7 +22,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "config.h"
 #endif
 
-#include "../qcommon/qcommon.h"
+#include "qcommon/qcommon.h"
 #include "winquake.h"
 #include <errno.h>
 #include <fcntl.h>
--- source/win32/qgl_win.c
+++ source/win32/qgl_win.c
@@ -33,7 +33,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #endif
 
 #include <float.h>
-#include "../ref_gl/r_local.h"
+#include "ref_gl/r_local.h"
 #include "glw_win.h"
 
 int   ( WINAPI * qwglChoosePixelFormat )(HDC, CONST PIXELFORMATDESCRIPTOR *);
