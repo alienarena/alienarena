931516fc9d264444bbbaed8975da480ca2e5bea1
blackice <blackice@example.com>
2010-09-19 09:24:16 +0000

    More autotools updates / dependency removal:  * Removed dependency on GLU    * Replaced gluBuild2DMipmaps with GL_GENERATE_MIPMAP (see http://www.opengl.org/wiki/Common_Mistakes#gluBuild2DMipmaps)    * Replaced gluLookAt with ad hoc implementation    * Updated configure.ac accordingly  * Fixed DGA-related bug introduced in the previous version Re-indented GL_Upload32

--- source/ref_gl/r_misc.c
+++ source/ref_gl/r_misc.c
@@ -24,7 +24,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #endif
 
 #include "r_local.h"
-#include <GL/glu.h>
 
 //For screenshots
 #if defined HAVE_JPEG_JPEGLIB_H
