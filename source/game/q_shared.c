4ba00a6ceae8ebf7922759164476206c25dc68b4
blackice <blackice@example.com>
2010-09-22 09:01:20 +0000

    Made the parameters of some of the string comparison functions constant

--- source/game/q_shared.c
+++ source/game/q_shared.c
@@ -1539,7 +1539,7 @@ void Com_PageInMemory (byte *buffer, int size)
 */
 
 // FIXME: replace all Q_stricmp with Q_strcasecmp
-int Q_stricmp (char *s1, char *s2)
+int Q_stricmp (const char *s1, const char *s2)
 {
 
 #if defined HAVE__STRICMP
@@ -1578,7 +1578,7 @@ int Q_strncasecmp (const char *s1, const char *s2, int n)
 	return 0;		// strings are equal
 }
 
-int Q_strcasecmp (char *s1, char *s2)
+int Q_strcasecmp (const char *s1, const char *s2)
 {
 	return Q_strncasecmp (s1, s2, 99999);
 }
--- source/game/q_shared.h
+++ source/game/q_shared.h
@@ -264,8 +264,8 @@ void Com_PageInMemory (byte *buffer, int size);
 //=============================================
 
 // portable case insensitive compare
-int Q_stricmp (char *s1, char *s2);
-int Q_strcasecmp (char *s1, char *s2);
+int Q_stricmp (const char *s1, const char *s2);
+int Q_strcasecmp (const char *s1, const char *s2);
 int Q_strncasecmp (const char *s1, const char *s2, int n);
 void Q_strcat (char *dst, const char *src, int dstSize);
 int Q_strnicmp (const char *string1, const char *string2, int n);
