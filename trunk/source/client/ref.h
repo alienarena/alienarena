a6440361bfffda92078ff86f903d417af85d1b64
irritant <irritant@example.com>
2010-10-26 04:50:25 +0000

    Precache all player models, their lods, and weapons.  Placed code for this in both client and server, so all should eventually be smooth no matter what client is used.

--- trunk/source/client/ref.h
+++ trunk/source/client/ref.h
@@ -370,6 +370,7 @@ struct image_s	*R_RegisterPic (char *name);
 
 void	R_SetSky (char *name, float rotate, vec3_t axis);
 
+void	R_RegisterBasePlayerModels(void);
 void	R_BeginRegistration (char *map);
 void	R_EndRegistration (void);
 
