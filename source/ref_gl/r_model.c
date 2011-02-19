908fee533dfc80dd3272149be7c3cb4e5417be16
irritant <irritant@example.com>
2011-02-19 06:24:36 +0000

    Revert last change, revert changes in pp.c

--- source/ref_gl/r_model.c
+++ source/ref_gl/r_model.c
@@ -703,9 +703,12 @@ void Mod_LoadTexinfo (lump_t *l)
 
 	for ( i=0 ; i<count ; i++, in++, out++)
 	{
-		for (j=0 ; j<8 ; j++)
+		for (j=0 ; j<4 ; j++)
+		{
+			/* note: treating as vecs[0][8] gives warning with gcc -O3 */
 			out->vecs[0][j] = LittleFloat (in->vecs[0][j]);
-
+			out->vecs[1][j] = LittleFloat (in->vecs[1][j]);
+		}
 		out->value = LittleLong (in->value);
 		out->flags = LittleLong (in->flags);
 		next = LittleLong (in->nexttexinfo);
