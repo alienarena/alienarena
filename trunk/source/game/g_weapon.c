3ec82478a149a42a06f58c8af5663d963c026af8
max <max@example.com>
2012-08-21 07:10:48 +0000

    Rockets are rendered fullbright. This looks a little better and is marginally faster as well.

--- trunk/source/game/g_weapon.c
+++ trunk/source/game/g_weapon.c
@@ -813,6 +813,7 @@ void fire_rocket (edict_t *self, vec3_t start, vec3_t dir, int damage, int speed
 	rocket->clipmask = MASK_SHOT;
 	rocket->solid = SOLID_BBOX;
 	rocket->s.effects |= EF_ROCKETEXHAUST ;
+	rocket->s.renderfx |= RF_FULLBRIGHT ;
 	if (!excessive->integer && self->client->invincible_framenum <= level.framenum)
 		//With too many rockets, lots of smoke trails hurt performance.
 		rocket->s.effects |= EF_ROCKET;
