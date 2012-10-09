da98bb858eee24d7ece97f5f02fd463fd6fb817b
max <max@example.com>
2012-10-09 01:56:07 +0000

    Rearrange declaration order in VB_BuildWorldVBO

--- trunk/source/ref_gl/r_vbo.c
+++ trunk/source/ref_gl/r_vbo.c
@@ -147,8 +147,8 @@ void VB_BuildSurfaceVBO(msurface_t *surf)
 void VB_BuildWorldVBO(void)
 {
 	msurface_t *surf;
-	int num_vertexes = totalVBObufferSize/7;
 	int i;
+	int num_vertexes = totalVBObufferSize/7;
 	currVertexNum = currElemNum = 0;
 	vbo_xyz_base = vbo_xyz_pos = ebo_pos = 0;
 	vbo_st_base = vbo_st_pos = num_vertexes*3*sizeof(float);
