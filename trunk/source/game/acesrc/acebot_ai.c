5251268ab1c82518e539a5d41b418fa4c7b3435a
irritant <irritant@example.com>
2013-07-30 14:57:51 +0000

    Update binaries. Fix bug with bots selecting violator in Tactical.

--- trunk/source/game/acesrc/acebot_ai.c
+++ trunk/source/game/acesrc/acebot_ai.c
@@ -1046,6 +1046,7 @@ void ACEAI_ChooseWeapon(edict_t *self)
 
 	// now go through normal weapon favoring routine
 	//  always favor the Vaporizor, unless close, then use the violator
+#ifndef TACTICAL
 	if ( range < 200.0f && self->skill > 0 )
 	{
 		selected = ACEIT_ChangeWeapon( self, FindItem( "Violator" ));
@@ -1053,6 +1054,7 @@ void ACEAI_ChooseWeapon(edict_t *self)
 		self->accuracy = 1.0f;
 		return;
 	}
+#endif
 	if ( self->skill > 1 )
 	{
 		if ( ACEIT_ChangeWeapon( self, FindItem( "Alien Vaporizer" )))
