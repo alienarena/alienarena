94102ca7e8f8f1ed6b34391859f3ea79a6ed74d4
max <max@example.com>
2014-02-01 03:41:39 +0000

    Work around performance issues with rocks and dynamic lights.

--- source/ref_gl/r_mesh.c
+++ source/ref_gl/r_mesh.c
@@ -221,7 +221,11 @@ void R_GetLightVals(vec3_t meshOrigin, qboolean RagDoll)
 			VectorCopy(meshOrigin, temp);
 			temp[2] += 24; //generates more consistent tracing
 
-			r_trace = CM_BoxTrace(temp, dl->origin, mins, maxs, r_worldmodel->firstnode, MASK_OPAQUE);
+			// TODO: Write CM_FastTrace!
+			if (strstr (currentmodel->name, "rock"))
+				r_trace.fraction = 1.0;
+			else
+				r_trace = CM_BoxTrace(temp, dl->origin, mins, maxs, r_worldmodel->firstnode, MASK_OPAQUE);
 
 			if(r_trace.fraction == 1.0)
 			{
