ba8c1447301fbf27757223d352f19f57f18e12c7
max <max@example.com>
2014-02-11 04:51:29 +0000

    Add gl_skyboxscale cvar for metalhead.

--- source/ref_gl/r_main.c
+++ source/ref_gl/r_main.c
@@ -1205,6 +1205,8 @@ void R_Register( void )
 	gl_picmip = Cvar_Get ("gl_picmip", "0", CVAR_ARCHIVE|CVARDOC_INT);
 	Cvar_Describe (gl_picmip, "Texture detail. 0 means full detail. Each higher setting has 1/4 less detail.");
 	gl_skymip = Cvar_Get ("gl_skymip", "0", 0);
+	gl_skyboxscale = Cvar_Get ("gl_skyboxscale", "1", CVAR_ARCHIVE|CVARDOC_FLOAT);
+	Cvar_Describe (gl_skyboxscale, "Move the skybox closer by setting this less than 1. Only use this if your hardware doesn't support a 24-bit depth buffer and the skybox is disappearing. Don't set this above 1.");
 	gl_showtris = Cvar_Get ("gl_showtris", "0", 0);
 	gl_showpolys = Cvar_Get ("gl_showpolys", "0", CVARDOC_INT);
 	Cvar_Describe (gl_showpolys, "Useful tool for mappers. 1 means show world polygon outlines for visible surfaces. 2 means show outlines for all surfaces in the PVS, even if they are hidden. FIXME: currently broken.");
