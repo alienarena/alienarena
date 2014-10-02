a755f57fe8728936e7d6f8a8b7f5c688847dee37
max <max@example.com>
2014-10-02 15:46:30 +0000

    Fix explosion distortion when MSAA is enabled. This fixes a regression introduced in r4407.

--- source/ref_gl/r_program.c
+++ source/ref_gl/r_program.c
@@ -1658,12 +1658,14 @@ static char fb_fragment_program[] = STRINGIFY (
 	void main(void)
 	{
 		vec2 noiseVec;
-		vec2 displacement;
+		vec4 displacement;
 		
 		noiseVec = normalize(texture2D(distortiontexture, gl_TexCoord[0].st)).xy;
-		displacement = gl_TexCoord[0].st + (noiseVec * 2.0 - vec2 (0.6389, 0.6339)) * intensity;
 		
-		gl_FragColor = texture2D (fbtexture, displacement);
+		// It's a bit of a rigomorole to do partial screen updates. This can
+		// go away as soon as the distort texture is a screen sized buffer.
+		displacement = gl_TexCoord[0] + vec4 ((noiseVec * 2.0 - vec2 (0.6389, 0.6339)) * intensity, 0, 0);
+		gl_FragColor = texture2D (fbtexture, (gl_TextureMatrix[0] * displacement).st);
 	}
 );
 
