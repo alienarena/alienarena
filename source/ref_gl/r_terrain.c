450ce95719212a4ad284b44fefdc1337c6255385
max <max@example.com>
2014-10-11 18:04:29 +0000

    Use malloc instead of hunk_alloc for mod->vboIDs. The 32-byte alignment of hunk_alloc on Windows makes that function unworkable for this purpose.

--- source/ref_gl/r_terrain.c
+++ source/ref_gl/r_terrain.c
@@ -88,9 +88,7 @@ void Mod_LoadTerrainModel (model_t *mod, void *_buf)
 	if (ndownward > 0)
 		Com_Printf ("WARN: %d downward facing polygons in %s!\n", ndownward, mod->name);
 	
-	mod->extradata = Hunk_Begin (3 * sizeof(*mod->vboIDs));
 	R_Mesh_LoadVBO (mod, MESHLOAD_CALC_NORMAL|MESHLOAD_CALC_TANGENT, basevbo, data.tri_indices, framevbo);
-	mod->extradatasize = Hunk_End ();
 	
 	CleanupTerrainData (&data);
 	Z_Free (framevbo);
