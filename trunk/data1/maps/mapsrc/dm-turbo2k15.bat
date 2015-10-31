e2f7ac43542d9d1f4ef5d61938b3dd4ceb133259
irritant <irritant@example.com>
2015-10-31 16:25:50 +0000

    Turbo updates.

--- trunk/data1/maps/mapsrc/dm-turbo2k15.bat
+++ trunk/data1/maps/mapsrc/dm-turbo2k15.bat
@@ -1,5 +1,4 @@
 qbsp3 dm-turbo2k15
-qvis3 -fast dm-turbo2k15
-//tqrad3 -direct 0.5 -entity 1.0 dm-dismal2k15
-qrad3 -direct 1.0 -entity 0.5 dm-turbo2k15
+qvis3 dm-turbo2k15
+tqrad3 -direct 1.0 -entity 1.0 dm-turbo2k15
 pause
