2d128c194b4e8969b8b4a597b7bdd15a194006ef
irritant <irritant@example.com>
2016-02-07 20:34:58 +0000

    Move all pre-gen3 maps and related files to the /arena folder. Move unused texture sets to /arena folder.

--- trunk/data1/maps/mapsrc/tac-impact_build.bat
+++ trunk/data1/maps/mapsrc/tac-impact_build.bat
@@ -1,4 +1,4 @@
 qbsp3 tac-impact
-qvis3 -fast tac-impact
-qrad3 -subdiv 128 -direct 1.0 -ambient 1.0 -entity 1.0 tac-impact
+qvis3 tac-impact
+tqrad3 -subdiv 128 -direct 1.0 -ambient 1.0 -entity 1.0 tac-impact
 pause
