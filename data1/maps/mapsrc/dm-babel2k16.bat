b97eee8cb022eebeb6968841098418f0a9d81c93
irritant <irritant@example.com>
2016-05-20 16:03:00 +0000

    Added Dm-Babel2k16

--- data1/maps/mapsrc/dm-babel2k16.bat
+++ data1/maps/mapsrc/dm-babel2k16.bat
@@ -1,3 +1,4 @@
-qbsp3 dm-babel2k16.map
-qvis3 -fast dm-babel2k16.map
-tqrad3 -direct 1.0 -entity 1.0 dm-babel2k16.map
+//qbsp3 dm-babel2k16.map
+//qvis3 dm-babel2k16.map
+//tqrad3 -ambient 3.0 dm-babel2k16.map
+qrad3 -texcheck -refine 4 -blur -ambient 3.0 dm-babel2k16
