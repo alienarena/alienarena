a169b2c56087417ad30ee535b613548cc55bf6ac
jar <jar@example.com>
2023-08-06 11:40:13 +0000

    Installer input validations

---
+++ trunk/Tools/AlienArenaInstaller/StringExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AlienArenaInstaller
+{
+	public static class StringExtensions
+	{
+		public static int ToInteger(this String text)
+		{
+			int.TryParse(text, out int intValue);
+			return intValue;
+		}
+	}
+}
