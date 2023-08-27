5a6f89a39df3eee85329c70dfa2d359d781e5f49
jar <jar@example.com>
2023-08-27 14:40:11 +0000

    Created tag 7.71.5 based on revision 5752

---
+++ tags/7.71.5/Tools/AlienArenaInstaller/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Alien Arena Installer")]
+[assembly: AssemblyDescription("Alien Arena Installer")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("alienarena.org")]
+[assembly: AssemblyProduct("Alien Arena Installer")]
+[assembly: AssemblyCopyright("Copyright © alienarena.org 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("860700e6-f1c1-40fb-aabd-c42293b8ad92")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.12")]
+[assembly: AssemblyFileVersion("1.0.0.12")]
